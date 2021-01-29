ce63fe7dd4872e217fc8389f03c468f96e95748b
Lucas <lcs.grc@outlook.com>
2021-01-29 17:15:00 +0100

    increase articles quantity within the cart : ongoing

--- frontend/public/javascript/cart.js
+++ frontend/public/javascript/cart.js
@@ -3,10 +3,17 @@ import { fetchArticles, price } from "./fetchArticles.js";
 const articleDOM = document.querySelector("article");
 const totalPriceDOM = document.querySelector("#total-price");
 const threeTimesPriceDOM = document.querySelector("#three-times-price");
-let deleteBtns, totalPrice, threeTimesPrice;
+const emptyCartDOM = document.querySelector("#empty-cart");
+const cartContentDOM = document.querySelector("#cart-content");
 const items = Object.values(localStorage).map((item) => JSON.parse(item));
 
+const displayCartContent = () => {
+  emptyCartDOM.classList.add("hidden");
+  cartContentDOM.classList.remove("hidden");
+};
+
 const displayItems = (articles) => {
+  displayCartContent();
   const article = articles[0];
   const item = JSON.parse(localStorage.getItem(article.name));
   const itemDOM = document.createElement("div");
@@ -25,20 +32,22 @@ const displayItems = (articles) => {
       </header>
       <form>
         <div class="group">
-          <label for="option">Option</label>
-          <select type="text">
+          <label>Option</label>
+          <select>
             <option>${item.lense}</option>
           </select>
         </div>
         <div class="group">
-          <label for="quantity">Quantité</label>
+          <label for="quantity-${article._id}">Quantité</label>
           <div class="row">
             <button class="btn btn-quantity quantity-remove" type="button">
               <i class="fas fa-minus"></i>
             </button>
-            <input id="quantity" type="number" min="1" max="10" value="${
-              item.quantity
-            }">
+            <input id="quantity-${
+              article._id
+            }" class="quantity" type="number" min="1" max="10" value="${
+    item.quantity
+  }">
             <button class="btn btn-quantity quantity-add" type="button">
               <i class="fas fa-plus"></i>
             </button>
@@ -61,13 +70,13 @@ items.forEach(async (item, i) => {
       .then(() => {
         // The if statement below is used to verify if all the articles are completely loaded
         if (i === items.length - 1) {
-          deleteBtns = document.querySelectorAll(".btn-delete");
-          totalPrice = items.reduce((acc, item) => {
+          const deleteBtns = document.querySelectorAll(".btn-delete");
+          const itemsDOM = document.querySelectorAll(".cart-item");
+          const totalPrice = items.reduce((acc, item) => {
             acc += item.price;
             return acc;
           }, 0);
-          threeTimesPrice = (1000 + totalPrice) / 3; // 1000 cents => 10 € administrative fees
-          console.log(threeTimesPrice);
+          const threeTimesPrice = (1000 + totalPrice) / 3; // 1000 cents => 10 € administrative fees
 
           totalPriceDOM.innerHTML = `${price(totalPrice).integer}€<sup>${
             price(totalPrice).decimal
@@ -86,6 +95,60 @@ items.forEach(async (item, i) => {
                 localStorage.removeItem(btn.dataset.name);
                 location.reload();
               }
+            });
+          });
+
+          itemsDOM.forEach((itemDOM) => {
+            //TODO : REPETITION from article.js
+            const itemQuantityDOM = itemDOM.querySelector(".quantity");
+            const quantityBtn = itemDOM.querySelectorAll(".btn-quantity");
+
+            const modifyValue = (value) => {
+              if (
+                +itemQuantityDOM.value < +itemQuantityDOM.min ||
+                (+itemQuantityDOM.value === +itemQuantityDOM.min &&
+                  value === +itemQuantityDOM.min)
+              ) {
+                itemQuantityDOM.value = itemQuantityDOM.min;
+              } else if (
+                +itemQuantityDOM.value > +itemQuantityDOM.max ||
+                (+itemQuantityDOM.value === +itemQuantityDOM.max &&
+                  value === +itemQuantityDOM.max)
+              ) {
+                itemQuantityDOM.value = itemQuantityDOM.max;
+              } else if (value === +itemQuantityDOM.min) {
+                +itemQuantityDOM.value--;
+              } else {
+                +itemQuantityDOM.value++;
+              }
+              itemQuantityDOM.dispatchEvent(new Event("change"));
+            };
+
+            const fixQuantity = () => {
+              if (+itemQuantityDOM.value < +itemQuantityDOM.min) {
+                itemQuantityDOM.value = itemQuantityDOM.min;
+              }
+              if (+itemQuantityDOM.value > +itemQuantityDOM.max) {
+                itemQuantityDOM.value = itemQuantityDOM.max;
+              }
+            };
+
+            const updatePrice = () => {
+              fixQuantity();
+              // price.double = (article.price / 100) * +itemQuantityDOM.value;
+              // articlePriceDOM.innerHTML = `${price.integer}€<sup>${price.decimal}</sup>`;
+            };
+
+            itemQuantityDOM.addEventListener("change", updatePrice);
+
+            quantityBtn.forEach((btn) => {
+              btn.addEventListener("click", () => {
+                if (btn.classList.contains("quantity-remove")) {
+                  modifyValue(+itemQuantityDOM.min);
+                } else {
+                  modifyValue(+itemQuantityDOM.max);
+                }
+              });
             });
           });
         }
